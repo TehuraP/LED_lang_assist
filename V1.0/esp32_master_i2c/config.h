43a2716f08659e35f1934894ff6b6b3a3f2cc696
AntonSaikia <antonsaikia11@gmail.com>
2024-09-09 12:22:03 +0200

    Folder renamed

---
+++ V1.0/esp32_master_i2c/config.h
@@ -0,0 +1,18 @@
+// config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIFI_SSID "UPC7956876"
+#define WIFI_PASSWORD "b4uwyccvcvMt"
+
+// Your PC's IP address
+#define HOST_NAME "http://192.168.0.221"
+
+// ":18080/get_data" for Crow Server
+// "/get_words.php" for Apache Server
+#define PATH_NAME "/get_words.php"
+
+
+// Add other sensitive information here
+
+#endif // CONFIG_H
--- led_vocab/led_vocab.ino
+++ led_vocab/led_vocab.ino
@@ -16,14 +16,12 @@
 const char *const eng[ARRAYSIZE] = { "airport", "why",  "noon", "difficult", "fruit", "other", "answer", "to travel", "to read", "funny",
          "assignment" , "Earth", "any", "condition", "solution","profession", "place" , "money", "beginning" , "yellow" ,
          "to get" , "to take", "train", "short", "airplane", "simple", "the same", "call" , "to be called", "tired", 
-         "sad", "finally" , "quietly", "to give" , "maybe", "cheap", "expensive", "window", "still", "always",
-         "where from", "key", "when", "who" , "a lot" , "exam", "right now", "outside", "to know" , "people"};
+         "sad", "finally" , "quietly", "to give" , "maybe", "cheap", "expensive", "window", "still", "always"};
 // German words array 
 const char *const deu[ARRAYSIZE] = { "der Flughafen", "warum",  "Mittag", "schwierig", "das Obst", "andere", "die Antwort",  "reisen", "lesen" , "lustig",
          "die Aufgabe", "die Erde", "jeder", "der Zustand", "die L^sung", "der Beruf" , "der Ort" , "das Geld" , "der Anfang" , "gelb",
          "erhalten", "nehmen", "der Zug", "kurz" ,"das Flugzeug" , "einfach", "gleich", "der Anruf", "hei`en", "m_de", 
-         "traurig", "endlich", "ruhig", "geben", "vielleicht", "billig", "teuer", "das Fenster", "noch" , "immer", 
-         "woher", "der Schl_ssel", "wann" , "wer", "viel" , "die Pr_fung", "gerade", "drau`en", "kennen", "die Leute"};
+         "traurig", "endlich", "ruhig", "geben", "vielleicht", "billig", "teuer", "das Fenster", "noch" , "immer"};
 DMD dmd(DISPLAYS_ACROSS,DISPLAYS_DOWN);
 
 void ScanDMD()
