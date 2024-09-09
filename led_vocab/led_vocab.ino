40c23022d8aaf196297fa01e6e6ef1427b26c23b
AntonSaikia <antonsaikia11@gmail.com>
2024-09-09 11:04:16 +0200

    minor changes

--- led_vocab/led_vocab.ino
+++ led_vocab/led_vocab.ino
@@ -1,27 +1,30 @@
 #include "SPI.h"      
 #include "DMD.h" 
 #include "TimerOne.h"
-//#include "Adafruit5x7.h"
+
 // you can remove the fonts if unused
+//#include "Adafruit5x7.h"
 #include "SystemFont5x7.h"
+
+/* change these values if you have more than one DMD connected */
 #define DISPLAYS_ACROSS 1   
 #define DISPLAYS_DOWN 1       
-/* change these values if you have more than one DMD connected */
+
 #define ARRAYSIZE 40
+
 // English translations array 
-const char *const eng[ARRAYSIZE] = { //"airport", "why",  "noon", "difficult", "fruit", "other", "answer", "to travel", "to read", "funny",
+const char *const eng[ARRAYSIZE] = { "airport", "why",  "noon", "difficult", "fruit", "other", "answer", "to travel", "to read", "funny",
          "assignment" , "Earth", "any", "condition", "solution","profession", "place" , "money", "beginning" , "yellow" ,
          "to get" , "to take", "train", "short", "airplane", "simple", "the same", "call" , "to be called", "tired", 
-         "sad", "finally" , "quietly", "to give" , "maybe", "cheap", "expensive", "sad", "still", "always",
+         "sad", "finally" , "quietly", "to give" , "maybe", "cheap", "expensive", "window", "still", "always",
          "where from", "key", "when", "who" , "a lot" , "exam", "right now", "outside", "to know" , "people"};
 // German words array 
-const char *const deu[ARRAYSIZE] = { //"der Flughafen", "warum",  "Mittag", "schwierig", "das Obst", "andere", "die Antwort",  "reisen", "lesen" , "lustig",
+const char *const deu[ARRAYSIZE] = { "der Flughafen", "warum",  "Mittag", "schwierig", "das Obst", "andere", "die Antwort",  "reisen", "lesen" , "lustig",
          "die Aufgabe", "die Erde", "jeder", "der Zustand", "die L^sung", "der Beruf" , "der Ort" , "das Geld" , "der Anfang" , "gelb",
          "erhalten", "nehmen", "der Zug", "kurz" ,"das Flugzeug" , "einfach", "gleich", "der Anruf", "hei`en", "m_de", 
-         "traurig", "endlich", "ruhig", "geben", "vielleicht", "billig", "teuer", "traurig", "noch" , "immer", 
+         "traurig", "endlich", "ruhig", "geben", "vielleicht", "billig", "teuer", "das Fenster", "noch" , "immer", 
          "woher", "der Schl_ssel", "wann" , "wer", "viel" , "die Pr_fung", "gerade", "drau`en", "kennen", "die Leute"};
 DMD dmd(DISPLAYS_ACROSS,DISPLAYS_DOWN);
-int i = 0;
 
 void ScanDMD()
 { 
@@ -43,7 +46,7 @@ void scrollText(String dispString, String dispString1)
   long timer2=start;
   boolean ret=false;
   int scroll_length = 5*max(sLength, sLength1);
-  Serial.print(scroll_length);
+
   int j = 0;
   while(j <= scroll_length ){
     if ( ( timer+20 ) < millis() ) {
@@ -58,14 +61,15 @@ void scrollText(String dispString, String dispString1)
 void setup()
 {
   Serial.begin(9600);
-   Timer1.initialize( 5000 );           
-/*period in microseconds to call ScanDMD. Anything longer than 5000 (5ms) and you can see flicker.*/
 
-   Timer1.attachInterrupt( ScanDMD );  
-/*attach the Timer1 interrupt to ScanDMD which goes to dmd.scanDisplayBySPI()*/
+  /*period in microseconds to call ScanDMD. Anything longer than 5000 (5ms) and you can see flicker.*/
+  Timer1.initialize( 5000 );  
 
-   dmd.clearScreen( true );            
-/* true is normal (all pixels off), false is negative (all pixels on) */
+  /*attach the Timer1 interrupt to ScanDMD which goes to dmd.scanDisplayBySPI()*/
+  Timer1.attachInterrupt( ScanDMD );  
+  
+  dmd.clearScreen( true );            
+  /* true is normal (all pixels off), false is negative (all pixels on) */
   
 }
 
@@ -75,7 +79,4 @@ void loop(){
   for (int i = 0; i < REPETITIONS; i++){
     scrollText(deu[id], eng[id]);
   }
-
-  
-
 }
